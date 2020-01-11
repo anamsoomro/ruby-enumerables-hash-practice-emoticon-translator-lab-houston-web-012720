64541eceb2eb8a4f22d5ade64c0a0feb397c3d89
anam soomro <anamsoomroed@gmail.com>
2020-01-11 21:53:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,18 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library (path)
+  dictionary = YAML.load_file(path)
+  result = {
+  :get_meaning => {},
+  #keys inside get_meaning are japanese emoticons that point to their meanings
+  :get_emoticon => {}
+  #keys inside :get_emoticon are english emoticons pointing to their japenese equivalents
+  }
+  dictionary.each do |meaning, emoticons|
+    result[:get_meaning][emoticons[1]] = meaning
+    result[:get_emoticon][emoticons[0]] = emoticons[1]
+  end
+  result
 end
 
 def get_japanese_emoticon
